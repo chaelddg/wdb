e612d2ea3b1bc33e5fd5a34ada1214ad0ffb5036
unknown <vinz_voilent@yahoo.com>
2024-02-01 08:24:23 +0800

    adding profile content

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,9 +1,102 @@
 import React from 'react';
+import prisma from '@/app/libs/prismadb';
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+
+export async function getSession() {
+  return await getServerSession(authOptions);
+}
 
 export default async function Profile() {
-  return (
-    <div>
-      Profile
-    </div>
-  )
+
+  
+  try {
+    const session = await getSession();
+
+    if (!session?.user?.email) {
+      return null;
+    }
+
+    const currentUser = await prisma.user.findUnique({
+      where: {
+        email: session.user.email
+      }
+    });
+
+    if (!currentUser) {
+      return null;
+    }
+
+    return (
+      <div>
+        {/* Check if currentUser exists before accessing its properties */}
+        {currentUser &&
+          <div class="bg-white max-w-2xl shadow overflow-hidden sm:rounded-lg mx-auto">
+          <img class="w-36 h-36 rounded-full mx-auto" src={currentUser.image || "/images/placeholder.jpg"} alt="Rounded avatar"></img>
+            <div class="px-4 py-5 sm:px-6">
+              <h3 class="text-lg leading-6 font-medium text-gray-900 text-center">
+              {currentUser.email}'s Profile
+              </h3>
+            </div>
+            <div class="border-t border-gray-200">
+              <dl>
+                <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                  <dt class="text-sm font-medium text-gray-500">
+                    Full name
+                  </dt>
+                  <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                  {currentUser.fname} {currentUser.mname} {currentUser.lname}
+                  </dd>
+                </div>
+                <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                  <dt class="text-sm font-medium text-gray-500">
+                    Gender
+                  </dt>
+                  <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                  {currentUser.gender}
+                  </dd>
+                </div>
+                <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                  <dt class="text-sm font-medium text-gray-500">
+                    Birthdate
+                  </dt>
+                  <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                  {currentUser.Birthdate}
+                  </dd>
+                </div>
+                <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                  <dt class="text-sm font-medium text-gray-500">
+                    Email address
+                  </dt>
+                  <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                  {currentUser.email}
+                  </dd>
+                </div>
+                <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                  <dt class="text-sm font-medium text-gray-500">
+                    Phone number
+                  </dt>
+                  <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                  {currentUser.PhoneNum}
+                  </dd>
+                </div>
+                <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                  <dt class="text-sm font-medium text-gray-500">
+                    Address
+                  </dt>
+                  <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                  {currentUser.Address}
+                  </dd>
+                </div>
+              </dl>
+            </div>
+          </div>}
+      </div>
+
+    )
+  } catch (error) {
+    return null;
+  }
+
+
 }
